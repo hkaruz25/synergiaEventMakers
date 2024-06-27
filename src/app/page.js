9cd9a4184d054e62c882bfad52c9421b8345a275
rezarinaldi <rez4rinaldi@gmail.com>
2024-06-27 22:48:13 +0700

    feat: adding prisma and model user

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
+      <h2>Test Fakhri 🔥</h2>
+      <Footer />
+    </main>
+  );
+}
