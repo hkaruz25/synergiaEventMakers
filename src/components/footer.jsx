817c0861f9c570d4c928e8313398c5900a19ca27
muhammadfr <fakhririzqullah168@gmail.com>
2024-06-28 16:18:49 +0700

    penambahan landing page dan daisy ui

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-4 text-center">
+      <div>
+        <p>synergia© All Rights Reserved.</p>
+        <p>Created with NextJS</p>
+      </div>
+    </footer>
+  );
 };
